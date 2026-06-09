22c427e103f16b4b6c41f0307ba2e9dac02fc2ab
frank <farruxvloging@gmail.com>
2026-06-10 03:17:17 +0900

    feat: HOMEWORK TASK Q

--- src/train.ts
+++ src/train.ts
@@ -1,3 +1,33 @@
+/*
+TASK Q:
+
+Shunday function yozing, u 2 ta parametrga ega bo'lib
+birinchisi object, ikkinchisi string bo'lsin.
+Agar qabul qilinayotgan ikkinchi string, objectning
+biror bir propertysiga mos kelsa, 'true', aks holda mos kelmasa 'false' qaytarsin.
+
+MASALAN: hasProperty({ name: "BMW", model: "M3" }, "model"); return true; 
+*/
+
+// Yechim:
+function hasProperty(obj: object, prop: string): boolean {
+    for (const key in obj) {
+        if (key === prop) {
+            return true;
+        }
+    }
+    return false;
+}
+
+console.log(hasProperty({ name: "BMW", model: "M3" }, "model"));
+
+
+
+
+
+
+
+
 /*   Project Standards:
 - Logging standards
 - Naming standards   
