70e8d5aa2e67721f382c6ca2e98c0ff03f2441f4
frank <farruxvloging@gmail.com>
2026-06-15 17:18:39 +0900

    feat: locals variables integration

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -3,14 +3,13 @@ import Errors from "../libs/Errors";
 import { T } from "../libs/types/common";
 import ProductService from "../models/Product.service";
 
-
-const productService = new ProductService()
+const productService = new ProductService();
 
 const productController: T = {};
 productController.getAllProducts = async (req: Request, res: Response) => {
   try {
     console.log("getAllProducts");
-    res.render("products")
+    res.render("products");
   } catch (err) {
     console.log("Error, getAllProducts:", err);
     if (err instanceof Errors) res.status(err.code).json(err);
