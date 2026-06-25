a4cd67c4d748b69e525b2c033ebab96878dd3fd9
frank <farruxvloging@gmail.com>
2026-06-26 07:02:52 +0900

    fix: users page render real data

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -108,7 +108,6 @@ restaurantController.getUsers = async (req: Request, res: Response) => {
   try {
     console.log("getLogin");
     const result = await memberService.getUsers();
-    console.log("result:", result);
 
     res.render("users", { users: result });
   } catch (err) {
