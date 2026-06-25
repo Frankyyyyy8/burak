0b4c836e4e247a3f103b812e33f9500ec0a10489
frank <farruxvloging@gmail.com>
2026-06-26 06:31:00 +0900

    feat: create new product frontend logic develop

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -32,6 +32,7 @@ productController.createNewProduct = async (
 ) => {
   try {
     console.log("createNewProduct");
+    console.log("req.body:", req.body);
     if (!req.files?.length)
       throw new Errors(HttpCode.INTERNAL_SERVER_ERROR, Message.CREATE_FAILED);
 
@@ -42,7 +43,7 @@ productController.createNewProduct = async (
 
     await productService.createNewProduct(data);
     res.send(
-      `<script> alert("Sucessful creation!"); window.location.replace('admin/product/all')</script>`,
+      `<script> alert("Sucessful creation!"); window.location.replace('/admin/product/all')</script>`,
     );
   } catch (err) {
     console.log("Error, createNewProduct:", err);
