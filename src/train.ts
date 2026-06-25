85b94c20eed0bb06477df420f2ff8602fa22cb82
frank <farruxvloging@gmail.com>
2026-06-26 05:48:30 +0900

    fix: modify products page based on products data

--- src/train.ts
+++ src/train.ts
@@ -1,4 +1,30 @@
 /**
+ TASK W
+
+Shunday function yozing, u o'ziga parametr sifatida
+yagona array va number qabul qilsin. Siz tuzgan function
+arrayni numberda berilgan uzunlikda kesib bo'laklarga
+ajratgan holatida qaytarsin.
+MASALAN: chunkArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3);
+return [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10]];
+
+Yuqoridagi namunada berilayotgan array ikkinchi parametr 3'ga
+asoslanib 3 bo'lakga bo'linib qaytmoqda. Qolgani esa o'z holati qolyapti
+ */
+
+// Yechim:
+function chunkArray(array: any[], size: number): any[][] {
+  const result: any[][] = [];
+
+  for (let i = 0; i < array.length; i += size) {
+    result.push(array.slice(i, i + size));
+  }
+
+  return result;
+}
+
+console.log(chunkArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3));
+/**
  TASK V
 
 Shunday function yozing, uni string parametri bo'lsin.
@@ -13,7 +39,7 @@ object sifatida qaytarilmoqda.
 */
 
 // Yechim:
-function countChars(str: string): { [key: string]: number } {
+/* function countChars(str: string): { [key: string]: number } {
   const result: { [key: string]: number } = {};
 
   for (const char of str) {
@@ -28,7 +54,7 @@ function countChars(str: string): { [key: string]: number } {
 }
 
 console.log(countChars("hello"));
-
+*/
 /**
  TASK U
 
